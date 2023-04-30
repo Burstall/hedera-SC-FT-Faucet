37b392c016e5b7087a44c30f2ec8a6f10300f99b
Burstall <richard@alphacenturi.com>
2023-04-30 10:04:00 +0100

    updated for new security model

--- contracts/FungibleFaucet.sol
+++ contracts/FungibleFaucet.sol
@@ -295,8 +295,9 @@ contract FungibleFaucet is HederaTokenService, Ownable {
 		require(!_paused, "Faucet is paused");
 		require(serials.length <= type(uint8).max, "Too many serials");
 		
+		// REMOVED: due ot Hedera security model change
 		// check if user has associated token
-		if(IERC721(_fungibleToken).balanceOf(msg.sender) == 0) associateToken(msg.sender, _fungibleToken);
+		// if(IERC721(_fungibleToken).balanceOf(msg.sender) == 0) associateToken(msg.sender, _fungibleToken);
 
 		// calc the amount to claim
 		// updates timestamps on call ahead of transfer to prevent rentrancy attack
